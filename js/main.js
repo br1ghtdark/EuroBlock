a8e29c1b0c1ebd83fc13d5ebbade91558d913388
br1ghtdark <marsha1l15@yandex.ru>
2023-07-14 00:02:59 +0300

    layout completed

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* =============================REVIEWS-SLIDER============================ */
+
+new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    slideClass: "reviews__slider-item",
+    navigation: {
+        nextEl: ".reviews__btn-next",
+        prevEl: ".reviews__btn-prev"
+    },
+    speed: 1300
+});
+
+/* =============================REVIEWS-SLIDER============================ */
